48d2cb2e6f06a2644f3741d9e7a0bbb1796ccf84
Israel Batista <israel.batista031@gmail.com>
2024-10-05 20:51:56 -0300

    Módulo 1 - criando o projeto

---
+++ src/app/components/Aside/Aside.jsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import styles from './aside.module.css'
+import logo from './logo.png'
+
+export function Aside() {
+    return (
+        <aside className={styles.aside}>
+            <Image
+                src={logo}
+                alt="Logo da Code Connect"
+                priority={true}
+            />
+        </aside>
+    )
+}
