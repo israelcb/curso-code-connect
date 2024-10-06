30e5366dd3610706d29041c1a7d8a180cb681743
Israel Batista <israel.batista031@gmail.com>
2024-10-06 00:43:06 -0300

    Módulo 4 - paginando dados

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
 import { CardPost } from "@/components/CardPost"
 import logger from "@/logger"
+import Link from "next/link"
+import styles from './page.module.css'
 
-async function getAllPosts() {
-  const response = await fetch('http://api:3042/posts')
-
+async function getAllPosts(page) {
+  const response = await fetch(
+    `http://api:3042/posts?_page=${page}&_per_page=6`
+  )
+  
   if (response.ok) {
     logger.info('Posts obtidos com sucesso')
     return response.json()
@@ -13,12 +17,18 @@ async function getAllPosts() {
   return []
 }
 
-export default async function Home() {
-  const posts = await getAllPosts()
+export default async function Home({ searchParams }) {
+  const page = searchParams?.page || 1
+  const { data: posts, prev, next } = await getAllPosts(page)
 
   return (
     <main className='grid'>
       {posts.map(p => <CardPost key={p.id} post={p} />)}
+
+      <div className={styles.links}>
+        {prev && <Link className="link" href={`/?page=${prev}`}>Página anterior</Link>}
+        {next && <Link className="link" href={`/?page=${next}`}>Próxima página</Link>}
+      </div>
     </main>
   )
 }
